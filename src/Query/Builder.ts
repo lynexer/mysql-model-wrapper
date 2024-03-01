d0b848d4db2ca748f5e01ab95c57536cedfee669
Kyle Graham <kylegraham05@gmail.com>
2024-03-01 08:51:42 -0800

    feat: Add where method to query builder

--- src/Query/Builder.ts
+++ src/Query/Builder.ts
@@ -1,10 +1,20 @@
 import { FieldPacket, RowDataPacket } from 'mysql2/promise';
 import { Connection } from '../Connection';
+import { InvalidArgumentException } from '../Support/Exceptions/InvalidArgumentException';
+
+type WhereInfo = {
+    type: string;
+    column: string;
+    operator: string;
+    value: any;
+    boolean: string;
+};
 
 export class Builder {
     protected connection: Connection;
     private table: string = '';
     private selects: string[] = ['*'];
+    private wheres: WhereInfo[] = [];
 
     public bitwiseOperators: string[] = ['&', '|', '^', '<<', '>>', '&~'];
     public operators: string[] = [
@@ -67,6 +77,43 @@ export class Builder {
         return this;
     }
 
+    public where(
+        column: string | [],
+        operator: any = null,
+        value: any = null,
+        boolean: string = 'and'
+    ): this {
+        let type = 'Basic';
+
+        if (typeof column === 'object') {
+            // TODO: Return array of wheres
+        }
+
+        column = column as string;
+
+        [value, operator] = this.prepareValueAndOperator(
+            value,
+            operator,
+            value === null && operator !== null
+        );
+
+        if (this.invalidOperatorAndValue(operator, value)) {
+            [value, operator] = [operator, '='];
+        }
+
+        if (value === null) {
+            // TODO: Return where null instead
+        }
+
+        if (this.isBitwiseOperator(operator)) {
+            type = 'Bitwise';
+        }
+
+        this.wheres.push({ type, column, operator, value, boolean });
+
+        return this;
+    }
+
     // TODO: Added Basic Where Clauses:
     // Where, OrWhere, WhereNot, WhereBetween, WhereIn,
     // WhereNotIn, WhereNull, WhereNotNull, WhereDate,
@@ -76,6 +123,30 @@ export class Builder {
         let query = `SELECT ${this.selects.join(',')} FROM ${this.table}`;
         console.log(query);
         return await this.connection.query(query);
+    }
+
+    protected prepareValueAndOperator(
+        value: string,
+        operator: any,
+        useDefault: boolean = false
+    ): string[] {
+        if (useDefault) {
+            return [operator, '='];
+        } else if (this.invalidOperatorAndValue(operator, value)) {
+            throw new InvalidArgumentException(
+                'Illegal operator and value combination.'
+            );
+        }
+
+        return [value, operator];
+    }
+
+    protected invalidOperatorAndValue(operator: string, value: any): boolean {
+        return (
+            value === null &&
+            operator in this.operators &&
+            !(operator in ['=', '<>', '!='])
+        );
     }
 
     protected invalidOperator(operator: string): boolean {
