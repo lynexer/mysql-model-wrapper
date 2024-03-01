5115ee524200b700f0aa063e6439e762e4eecc48
Kyle Graham <kylegraham05@gmail.com>
2024-02-29 17:10:30 -0800

    feat: Create Mixins support handler

---
+++ src/Support/Mixins.ts
@@ -0,0 +1,14 @@
+export namespace Mixins {
+    export function apply(derivedCtor: any, constructors: any[]): void {
+        constructors.forEach(baseCtor => {
+            Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+                Object.defineProperty(
+                    derivedCtor.prototype,
+                    name,
+                    Object.getOwnPropertyDescriptor(baseCtor.prototype, name) ||
+                        Object.create(null)
+                );
+            });
+        });
+    }
+}
