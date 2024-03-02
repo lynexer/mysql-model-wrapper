ff95f381e22e8e4875519bd1bc11966c972fecd7
Kyle Graham <kylegraham05@gmail.com>
2024-03-01 21:40:57 -0800

    feat: Add table prefix accessor and mutator

--- src/Query/MySqlGrammar.ts
+++ src/Query/MySqlGrammar.ts
@@ -10,5 +10,16 @@ export class MySqlGrammar {
     protected operators: string[] = ['sounds like'];
     protected bitwiseOperators: string[] = [];
     protected tablePrefix: string = '';
+
     protected selectComponents: string[] = ['selects', 'table', 'wheres'];
+
+    public getTablePrefix(): string {
+        return this.tablePrefix;
+    }
+
+    public setTablePrefix(prefix: string): this {
+        this.tablePrefix = prefix;
+
+        return this;
+    }
 }
