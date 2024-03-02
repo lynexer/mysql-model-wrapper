ea3add58595e1c4b281161ea4f38990c17f27756
Kyle Graham <kylegraham05@gmail.com>
2024-03-01 21:49:08 -0800

    feat: Add default query grammar to connection

--- src/Connection.ts
+++ src/Connection.ts
@@ -5,20 +5,34 @@ import {
     RowDataPacket,
     createPool
 } from 'mysql2/promise';
+import { MySqlGrammar } from './Query/MySqlGrammar';
 
 export class Connection {
     private credentials: PoolOptions;
     private conn: Pool;
+    protected queryGrammar: MySqlGrammar;
 
     constructor(credentials: PoolOptions) {
         this.credentials = credentials;
         this.conn = createPool(this.credentials);
+
+        this.queryGrammar = new MySqlGrammar();
     }
 
     private ensureConnection() {
         if (!this?.conn) {
             this.conn = createPool(this.credentials);
         }
+    }
+
+    public getQueryGrammar(): MySqlGrammar {
+        return this.queryGrammar;
+    }
+
+    public setQueryGrammer(grammar: MySqlGrammar): this {
+        this.queryGrammar = grammar;
+
+        return this;
     }
 
     public async query(sql: string): Promise<[RowDataPacket[], FieldPacket[]]> {
